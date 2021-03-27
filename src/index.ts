ccab87a31b841209c95138ffde089f7032dac648
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-27 23:44:16 +0900

    UPDATE Add Block calculateBlockHash method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { v4 } from "uuid";
+import CryptoJS from "crypto-js";
 
 class Block {
   constructor(
@@ -8,6 +8,12 @@ class Block {
     public data: string,
     public timestamp: number
   ) {}
+
+  static calculateBlockHash(index: number, previousHash: string, timestamp: number, data: string): string {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  } 
 }
 
-const genesisBlock: Block = new Block(0, v4(), "", "hello", (+new Date));
+const genesisBlock: Block = new Block(0, "asklfjaskldfjsla", "", "hello", (+new Date));
+
+const blockChain: Block[] = [genesisBlock];
